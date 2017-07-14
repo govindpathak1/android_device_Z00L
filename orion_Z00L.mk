a8c8699e5999da5ac574cf6518a723b6c5405aa9
Govind Pathak <govindpathak122@gmail.com>
2017-07-14 16:50:51 +0530

    orion-ify

--- orion_Z00L.mk
+++ orion_Z00L.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/asus/Z00L/full_Z00L.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
-PRODUCT_NAME := aoscp_Z00L
+PRODUCT_NAME := orion_Z00L
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z00L
